6b7a33d0c1fbf9ccd018c0710a48ad3557330df9
angesalazars <angelicasalazar2828@gmail.com>
2024-11-15 12:07:01 +0100

    ADDED EXTRA ELEMENTS AND CORRECTED SOME COMMANDS
    MADE THE GRASS AND BOWLS SHOW AGAIN BY RE ARRANGING AND CHANGING SIZES

--- game.js
+++ game.js
@@ -1,53 +1,49 @@
 
+// variables 
+let bowlX = -340;
+let bowlY = 400;
+let grassX = 0;
+let grassY = 570; 
+let characterX = 280;
+let characterY = 30;
+let velocityY = 0.2; 
+let acceleration = 0.1;
+let gameState = true; 
+
 
 function setup() {
   createCanvas(800, 600);
 }
 
-function draw() {
-  background(0, 255, 255);
-  rainbow(175, 260);
-  character(260, 120);
-  grass(grassX, grassY);
-  // bowlLeprechaun(bowlX, bowlY);
-  for (let i = 0; i < 3; i ++) {
-    bowlLeprechaun (i * 500, bowlY);
-  } 
 
-}
-
-//rainbow
+//character, background and elements 
 function rainbow(x, y) {
   fill(255, 0, 0);
   noStroke();
-  rect(characterX-100, characterY-230, 440, 20);
+  rect(280-100, 400-230, 440, 20);
   fill(255, 160, 20);
-  rect(characterX-100, characterY-210, 440,20);
+  rect(280-100, 400-210, 440,20);
   fill(255, 255, 0);
-  rect(characterX-100, characterY-190, 440,20);
+  rect(280-100, 400-190, 440,20);
   fill(0, 255, 0);
-  rect(characterX-100, characterY-170, 440,20);
+  rect(280-100, 400-170, 440,20);
   fill(0, 0, 255);
-  rect(characterX-100, characterY-150, 440,20);
+  rect(280-100, 400-150, 440,20);
   fill(80, 0, 80);
-  rect(characterX-100, characterY-130, 440,20);
+  rect(280-100, 400-130, 440,20);
   fill(255,255,255);
-  ellipse(characterX-120, characterY-220, 100, 100);
+  ellipse(280-120, 400-220, 100, 100);
   fill(255,255,255);
-  ellipse(characterX-120, characterY-140, 100, 100);
+  ellipse(280-120, 400-140, 100, 100);
   fill(255,255,255);
-  ellipse(characterX-180, characterY-180, 100, 100);
+  ellipse(280-180, 400-180, 100, 100);
   fill(255,255,255);
-  ellipse(characterX+340, characterY-220, 100, 100);
+  ellipse(280+340, 400-220, 100, 100);
   fill(255,255,255);
-  ellipse(characterX+340, characterY-140, 100, 100);
+  ellipse(280+340, 400-140, 100, 100);
   fill(255,255,255);
-  ellipse(characterX+400, characterY-180, 100, 100);
+  ellipse(280+400, 400-180, 100, 100); 
   }
-
-//character 
-let characterX = 280;
-let characterY = 400;
 
 function character(x,y){
 
@@ -55,7 +51,7 @@ function character(x,y){
 fill(255, 128, 0);
 push();
 translate(x, y);
-scale(0.5);
+scale(0.4);
 noStroke();
 beginShape();
 vertex(x+5, y+200);
@@ -129,7 +125,7 @@ arc(x+5, y+90, 80, 50, 0, PI);
 //cheeks
 //left eye
 fill(255, 189, 223);
-push();
+// push();
 translate(1, 1);
 ellipse(x-60, y+30, 40, 30);
 ellipse(x+80, y+30, 40, 30);
@@ -137,92 +133,85 @@ pop();
 
 }
 
-//grass
-let grassX = 0;
-let grassY = 570; 
-
 function grass (x, y) {
   fill(0, 255, 0);
-  rect(grassX-520, grassY+360, 1700, 30);
+  rect(x, y+50, 1700, 30);
 }
-
-//buckets
-let bowlX = -340;
-let bowlY = 700;
-
 
 function bowlLeprechaun(x, y) {
   fill(0, 0, 0);
   stroke(80, 80, 80);
   strokeWeight(6);
-  ellipse (x-220, y+100, 300, 300);
+  ellipse (x+200, y+130, 100, 100);
   fill(0, 0, 0);
   stroke(80, 80, 80);
   strokeWeight(6);
-  rect(x-400, y-70, 350, 70);
- 
+  rect(x+150, y+80, 100, 20);
+  fill (255, 0, 0);
+  textStyle(BOLD);
+  textSize(50);
+  text("x", 187, 423, 100, 100);
+  fill (255, 0, 0);
+  textStyle(BOLD);
+  textSize(50);
+  text("x", 587, 423, 100, 100);
+  fill (105, 195, 100);
+  textStyle(BOLD);
+  textSize(20);
+  text("SLAY", 373, 423, 100, 100);
+  fill (105, 195, 100);
+  textStyle(BOLD);
+  textSize(20);
+  text("QUEEN", 365, 450, 100, 100);
 }
+
+
  
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fill(0, 0, 0);
-// rect(x-15, y-140, 40, 22);
-
-
 /* THE GAME- CHARACTER DESCRIPTION
 - Irish Leprechaun 
 -Rainbow 
--Goodies bucket to win 
--Trash buckets to loose 
+-money bucket to win 
+-'x' buckets to loose 
 
 SOME MECHANICS 
 -Tap the screen to make the character go up
 -Take gravity into account
 -End the game if arriving into trash instead of goodies bucket
+-win if ending in money
 */
 
 //This code was taken from master' students example from one of the lectures (flappy_ufo)
 
-//game logistics variable 
-// let velocityY = 0.2;
-// let acceleration = 0.2;
+//make it fly
 
-// // game state variable 
-// let gameState = true;
+function mechanichs() {
+// character
+character(characterX, characterY);
+//game state
+if(gameState === true) {
 
-// //gravity logic 
-// characterY = characterY + velocityY;
-// velocityY = velocityY + acceleration;
+   //gravity logic
+characterY = characterY + velocityY;
+velocityY = velocityY + acceleration;
+}
+//decrease the velocity when clicking
+if (mouseIsPressed) {
+  velocityY = velocityY - 0.7;                                          
+} 
 
-// //decrease the velocity when clicking
-// if (mouseIsPressed){
-//   velocityY = velocityY - 0.7;
-// }
+if (characterY >200) {
+  gameState = false;
+}
 
-// // ends game when character touches the ground
-// if (characterY > 180) {
-//   gameState = false;
-//   console.log('failed');
-// }
+}
+ 
+function draw() {
+  background(0, 255, 255);         
+  rainbow(175, 260);    
+  grass(0, 520); 
+  for (let i = 0; i < 3; i ++) {
+    bowlLeprechaun (i * 200, bowlY);
+  } 
+  mechanichs (); 
+}
 
